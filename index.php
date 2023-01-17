67e7c6ef80fe79bcf067ea4a27b8702c217011cc
LucaConiglio <lucaconiglio.92@gmail.com>
2023-01-17 09:54:22 +0100

    da terminare

--- index.php
+++ index.php
@@ -42,6 +42,21 @@ var_dump($card);
  </head>
  <body>
   <?php echo $card->getAnimale()?>
+  <section>
+    <div class="container">
+      <div class="d-flex justify-content-around p5">
+        <div foreach="Prodotti as prodotto" class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </section>
   
 
  </body>
